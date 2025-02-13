1a32b41c00b22a91a9968abfffee2bd63974cf4a
Vardhan1536 <balavardhantummalacherla@gmail.com>
2025-02-13 23:12:23 +0530

    up

--- src/backend/main.py
+++ src/backend/main.py
@@ -300,7 +300,7 @@ async def get_chat(request : ChatRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
     
